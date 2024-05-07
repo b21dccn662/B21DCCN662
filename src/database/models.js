246d7986caba6ebef22ebbfc4594114c09ffc86e
B21DCCN662 - Phạm Thành Thái <162034697+b21dccn662@users.noreply.github.com>
2024-05-07 14:07:12 +0700

    models.js

--- src/database/models.js
+++ src/database/models.js
@@ -22,6 +22,38 @@ const models = [
         math: 11.0,
         liter: 11.0,
         eng: 11.0
-    }
+    },
+    {
+        id: "B21DCDT1289",
+        name: "Hoàng Mạnh Quỳnh",
+        dob: "12/07/2003",
+        math: 1.0,
+        liter: 1.0,
+        eng: 1.0
+    },
+    {
+        id: "B21DCDT097",
+        name: "Lương Đức Hòa",
+        dob: "02/01/2004",
+        math: 2.0,
+        liter: 2.0,
+        eng: 2.0
+    },
+    {
+        id: "B21DCCN815",
+        name: "Louksone SIHALATH",
+        dob: "01/06/2001",
+        math: 9.0,
+        liter: 9.0,
+        eng: 9.0
+    },
+    {
+        id: "B21DCDT230",
+        name: "Trần Hữu Tú",
+        dob: "12/05/2003",
+        math: 8.0,
+        liter: 8.0,
+        eng: 8.0
+    },
 ]
 export default models;
